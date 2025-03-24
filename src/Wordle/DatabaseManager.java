48f9ff3d902404f49dceab040c9719357a81ff48
grudzinskio <grudzinskio@msoe.edu>
2025-03-24 14:43:58 -0500

    Made it so it automatically copies your path for identifier - updated the kind of path

--- src/Wordle/DatabaseManager.java
+++ src/Wordle/DatabaseManager.java
@@ -7,8 +7,7 @@ import java.sql.SQLException;
 public class DatabaseManager {
 
 
-    private static final String HOME_DIR = System.getProperty("user.home");
-    private static final String DB_PATH = HOME_DIR + "\\IdeaProjects\\s25-flynn-group15\\identifier.sqlite";
+    private static final String DB_PATH = "identifier.sqlite";
     private static final String URL = "jdbc:sqlite:" + DB_PATH;
 
     public static Connection getConnection() throws SQLException {
